326d09ba71cc9fe0fea613c69a35785401aa10c8
allenx <mint.mr@gmail.com>
2017-03-07 22:10:39 +0800

    Everest To Make WePeiYang Great Again.

---
+++ WePeiYang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WePeiYang
+//
+//  Created by Allen X on 3/7/17.
+//  Copyright © 2017 twtstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
