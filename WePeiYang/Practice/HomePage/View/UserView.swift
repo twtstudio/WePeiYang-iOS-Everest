417c518cb4c9e91aa83c63251b0f73748774ec06
JasonEWNL <1006091953@qq.com>
2018-07-25 19:49:12 +0800

    Using UIScrollView to reconfiguration HomeView by JasonEWNL.

--- WePeiYang/Practice/HomePage/View/UserView.swift
+++ WePeiYang/Practice/HomePage/View/UserView.swift
@@ -10,21 +10,8 @@ import UIKit
 
 class UserView: UIView {
     
-    static func addCell(withImage image: UIImage, andText text: String, andY y: CGFloat) -> UIButton {
-        
-        let button = UIButton(frame: CGRect(x: deviceHeight/48, y: y, width: deviceWidth - deviceHeight/24, height: deviceHeight/12))
-        button.setImage(image, for: .normal)
-        button.setTitle(text, for: .normal)
-        
-        return button
-    }
-    
-    // let practiceStatistics = addCell(withImage: #imageLiteral(resourceName: "顺序练习"), andText: "练习统计", andY: deviceHeight/2)
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.clear
-        // self.addSubview(practiceStatistics)
     }
     
     required init?(coder aDecoder: NSCoder) {
