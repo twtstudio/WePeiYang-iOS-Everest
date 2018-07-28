2548394e23f4b354ca5e0a2ddb92c458ec056f9d
JasonEWNL <1006091953@qq.com>
2018-07-28 17:04:44 +0800

    Complete UserView UI committed by JasonEWNL.

--- WePeiYang/Practice/HomePage/Controller/PracticeHomeViewController.swift
+++ WePeiYang/Practice/HomePage/Controller/PracticeHomeViewController.swift
@@ -8,33 +8,22 @@
 
 import UIKit
 
-class PracticeHomeViewController: UIViewController, UIScrollViewDelegate {
+class PracticeHomeViewController: UIViewController {
     
     let headView = HeadView()
-    let userView = UserView()
-    let homeView = HomeView()
+    
     let contentScrollView = UIScrollView()
+    
+    let userTableView = UITableView(frame: CGRect(), style: .grouped) // UserView()
+    let UserViewCellTitle = ["练习历史", "我的错题", "我的收藏", "我的上传"]
+    let UserViewCellIcon = [#imageLiteral(resourceName: "practiceHistory"), #imageLiteral(resourceName: "practiceWrong"), #imageLiteral(resourceName: "practiceCollection"), #imageLiteral(resourceName: "practiceUpload")]
+    
+    let homeView = HomeView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
-        
-        contentScrollView.delegate = self
-        
-        contentScrollView.contentSize = CGSize(width: 2 * deviceWidth, height: contentScrollView.frame.height)
-        contentScrollView.contentOffset = CGPoint(x: deviceWidth, y: 0)
-        
-        contentScrollView.showsHorizontalScrollIndicator = false
-        contentScrollView.showsVerticalScrollIndicator = false
-        contentScrollView.isPagingEnabled = true
-        
-        self.view.addSubview(contentScrollView)
-        
-        userView.frame = CGRect(x: deviceWidth, y: 0, width: deviceWidth, height: contentScrollView.frame.height)
-        contentScrollView.addSubview(userView)
-        
-        homeView.frame = CGRect(x: 0, y: 0, width: deviceWidth, height: contentScrollView.frame.height)
-        contentScrollView.addSubview(homeView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,7 +41,7 @@ class PracticeHomeViewController: UIViewController, UIScrollViewDelegate {
         
         /* 标题 */
         let titleLabel = UILabel(text: "天外天刷题")
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 21.0)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 21)
         titleLabel.textColor = .white
         titleLabel.sizeToFit()
         navigationItem.titleView = titleLabel
@@ -69,7 +58,27 @@ class PracticeHomeViewController: UIViewController, UIScrollViewDelegate {
 
         /* 滑动视图 */
         contentScrollView.frame = CGRect(x: 0, y: headView.frame.size.height, width: deviceWidth, height: deviceHeight - barHeight - headView.frame.height)
+        contentScrollView.delegate = self
         
+        contentScrollView.contentSize = CGSize(width: 2 * deviceWidth, height: contentScrollView.frame.height)
+        contentScrollView.contentOffset = CGPoint(x: deviceWidth, y: 0)
+        
+        contentScrollView.showsHorizontalScrollIndicator = false
+        contentScrollView.showsVerticalScrollIndicator = false
+        contentScrollView.isPagingEnabled = true
+        
+        self.view.addSubview(contentScrollView)
+        
+        /* "我的" 视图 */
+        userTableView.frame = CGRect(x: deviceWidth, y: 0, width: deviceWidth, height: contentScrollView.frame.height)
+        userTableView.backgroundColor = .clear
+        userTableView.delegate = self
+        userTableView.dataSource = self
+        contentScrollView.addSubview(userTableView)
+        
+        /* "题库" 视图 */
+        homeView.frame = CGRect(x: 0, y: 0, width: deviceWidth, height: contentScrollView.frame.height)
+        contentScrollView.addSubview(homeView)
     }
     
     // 点击按钮切换, 改变白色指示条位置与按钮可用状态 //
@@ -91,9 +100,19 @@ class PracticeHomeViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    // 进入搜索界面 //
+    @objc func practiceSearch() {
+        
+    }
+    
+}
+
+/* 滑动视图代理 */
+extension PracticeHomeViewController: UIScrollViewDelegate {
+    
     // 正在滑动时, 动态改变白色指示条位置 //
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        headView.underLine.frame.origin.x = deviceWidth / 6 + deviceWidth / 2 * (scrollView.contentOffset.x / deviceWidth)
+        headView.underLine.frame.origin.x = deviceWidth / 6 + deviceWidth / 2 * (self.contentScrollView.contentOffset.x / deviceWidth)
     }
     
     // 结束滑动时, 判断位置刷新按钮可用状态 //
@@ -102,12 +121,111 @@ class PracticeHomeViewController: UIViewController, UIScrollViewDelegate {
             (self.headView.userOptionButton.isEnabled, self.headView.homeOptionButton.isEnabled) = (true, false)
         } else {
             (self.headView.userOptionButton.isEnabled, self.headView.homeOptionButton.isEnabled) = (false, true)
-        
         }
     }
     
-    // 进入搜索界面 //
-    @objc func practiceSearch() {
+}
+
+/* 表单视图数据 */
+extension PracticeHomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch tableView {
+        
+        // "我的" 视图 - 单元个数 //
+        case userTableView:
+            return UserViewCellTitle.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        
+        switch tableView {
+        
+        // "我的" 视图 - 单元 //
+        case userTableView:
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "UserViewCell")
+            
+            // 自定义系统默认 UITableViewCell 的 imageView 大小 //
+            cell.imageView?.image = UserViewCellIcon[row]
+            UIGraphicsBeginImageContextWithOptions(CGSize(width: 32, height: 32), false, UIScreen.main.scale)
+            cell.imageView?.image?.draw(in: CGRect(x: 0, y: 0, width: 32, height: 32))
+            cell.imageView?.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            cell.textLabel?.text = UserViewCellTitle[row]
+            cell.accessoryType = .disclosureIndicator
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+}
+
+/* 表单视图代理 */
+extension PracticeHomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch tableView {
+            
+        // "我的" 视图 - 单元高度 //
+        case userTableView:
+            return 44
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch tableView {
+            
+        // "我的" 视图 - 头视图高度 //
+        case userTableView:
+            return section == 0 ? 300 : 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch tableView {
+            
+        // "我的" 视图 - 头视图 //
+        case userTableView:
+            if section != 0 { return nil }
+            
+            let userView = UserView()
+            userView.userHeadView.sd_setImage(with: URL(string: TwTUser.shared.avatarURL ?? ""), placeholderImage: UIImage(named: "account_circle")!.with(color: .gray))
+
+            if TwTUser.shared.token != nil {
+                userView.userNameLabel.text = TwTUser.shared.username
+                userView.userTitleLabel.text = "刷题小火箭" // 称号
+                userView.practicedQuestionNumber.text = "1010" // 已练习题目数
+                userView.practicedCourseNumber.text = "10" // 已练习科目数
+                
+                let correctRateText = NSMutableAttributedString(string: "正确率 98%") // 正确率
+                correctRateText.addAttribute(.foregroundColor, value: UIColor.darkGray, range: NSMakeRange(0, 4))
+                userView.correctRate.attributedText = correctRateText
+            } else {
+                userView.userNameLabel.text = "我的昵称"
+                userView.userTitleLabel.text = "我的称号"
+                userView.practicedQuestionNumber.text = "0"
+                userView.practicedCourseNumber.text = "0"
+                
+                let correctRateText = NSMutableAttributedString(string: "正确率 0%")
+                correctRateText.addAttribute(.foregroundColor, value: UIColor.darkGray, range: NSMakeRange(0, 4))
+                userView.correctRate.attributedText = correctRateText
+            }
+            
+            return userView
+        default:
+            return nil
+        }
         
     }
     
