bf9d32fe9787319823fc3195dda51a7c9d00c10f
uareagay <1919537704@qq.com>
2018-11-20 02:45:50 +0800

    热门课程列表雏型

--- WePeiYang/Classtable/Controller/AuditHomeViewController.swift
+++ WePeiYang/Classtable/Controller/AuditHomeViewController.swift
@@ -250,3 +250,91 @@ extension AuditHomeViewController: UITableViewDataSource {
         }
     }
 }
+
+extension AuditHomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "热门课程"
+        } else if section == 1 {
+            return "我的蹭课"
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 40
+        } else {
+            return 30
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+       return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 60
+        } else {
+            return 120
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        if section == 0 {
+            return UIView()
+        } else {
+            return UIView()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if isFold == true, indexPath.section == 0, indexPath.row == 3 {
+            self.isFold = false
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension AuditHomeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            if self.isFold == true {
+                return self.popularList.count == 0 ? 0 : 3 + 1
+            } else {
+                return self.popularList.count
+            }
+        } else {
+            return 6
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            if isFold == true {
+                if indexPath.row < 3 {
+                    let cell = UITableViewCell(style: .default, reuseIdentifier: "CELL_default")
+                    cell.textLabel?.text = self.popularList[indexPath.row].course.name
+                    return cell
+                } else {
+                    let cell = UITableViewCell(style: .default, reuseIdentifier: "CELL_default")
+                    cell.textLabel?.text = "展开热门课程"
+                    cell.textLabel?.textColor = .red
+                    return cell
+                }
+            } else {
+                let cell = UITableViewCell(style: .default, reuseIdentifier: "CELL_default")
+                cell.textLabel?.text = self.popularList[indexPath.row].course.name
+                cell.imageView?.image = UIImage.resizedImage(image: UIImage(named: "readerAvatar0")!, scaledToSize: CGSize(width: 12, height: 12))
+                return cell
+            }
+        } else {
+            return UITableViewCell()
+        }
+    }
+}
