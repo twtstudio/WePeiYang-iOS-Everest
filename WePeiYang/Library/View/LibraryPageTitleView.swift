9ab75c5bfa93bdb572fc8969098724d4d1da34f7
Maoxian <Maoxian@github.com>
2018-11-24 17:39:18 +0800

    Library Module

---
+++ WePeiYang/Library/View/LibraryPageTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  LibraryPageTitleView.swift
+//  WePeiYang
+//
+//  Created by 毛线 on 2018/11/24.
+//  Copyright © 2018 twtstudio. All rights reserved.
+//
+
+import UIKit
+
+class LibraryPageTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
