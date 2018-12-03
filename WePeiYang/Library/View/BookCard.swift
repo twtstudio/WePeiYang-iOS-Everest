6f32a571e876a2c53062669911a868ef64513235
Maoxian <Maoxian@github.com>
2018-12-03 11:50:54 +0800

    crash fixed

--- WePeiYang/Library/View/BookCard.swift
+++ WePeiYang/Library/View/BookCard.swift
@@ -126,44 +126,37 @@ class BookCard: CardView {
     }
 }
 
-extension BookCard {
-    func calculateDay(returnTime: String) -> Int {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let returnDate = dateFormatter.date(from: returnTime)
-        let between = Calendar.current.dateComponents([.day], from: Date(), to: returnDate!)
-        return between.day ?? 0
-    }
-}
-
 extension LibraryBook {
     static func getImage(id: String, success: ((String) -> Void)? = nil, failure: @escaping () -> Void) {
         SolaSessionManager.solaSession(url: "/library/book/" + id, success: { dict in
-            let data = dict["data"] as? [String: Any]
-            if let isbn = data!["isbn"] as? String {
-                self.sendIsbn(parameters: ["isbns": isbn], success: {
-                    imageDict in
-                    if let result = imageDict["result"] as? [[String: Any]] {
-                        if !(result.isEmpty) {
-                            if let link = result[0]["coverlink"] {
-                                success?(link as! String)
-                                return
-                            }
-                        }
-                    }
+            guard let data = dict["data"] as? [String: Any],
+                let isbn = data["isbn"] as? String else {
                     failure()
-                }, failure: {
-                    _ in
-                    failure()
-                })
+                    return
             }
+            self.sendIsbn(parameters: ["isbns": isbn], success: {
+                imageDict in
+                guard let result = imageDict["result"] as? [[String: Any]] else {
+                    failure()
+                    return
+                }
+                if !(result.isEmpty) {
+                    guard let link = result[0]["coverlink"] else {
+                        failure()
+                        return
+                    }
+                    success?(link as? String ?? "")
+                }
+            }, failure: { _ in
+                failure()
+            })
         }, failure: { _ in
             failure()
         })
     }
     
-    static func sendIsbn(parameters: [String: String]? = nil, success: @escaping ([String: Any]) -> Void, failure: @escaping (Error) -> Void) {
-        let isbn = parameters!["isbns"] ?? ""
+    static func sendIsbn(parameters: [String: String], success: @escaping ([String: Any]) -> Void, failure: @escaping (Error) -> Void) {
+        let isbn = parameters["isbns"] ?? ""
         Alamofire.request("https://vote.twtstudio.com/getImgs.php?isbns=" + "\(isbn)", method: .post, parameters: parameters, headers: nil).responseJSON { response in
             switch response.result {
             case .success:
