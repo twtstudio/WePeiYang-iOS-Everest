c81d74fe5d178d14e1926655aaeaceac21ccc482
uareagay <1919537704@qq.com>
2018-12-16 02:13:17 +0800

    补充修复课程表小圆点bug

--- WePeiYang/Classtable/Controller/ClassTableViewController.swift
+++ WePeiYang/Classtable/Controller/ClassTableViewController.swift
@@ -426,7 +426,7 @@ extension ClassTableViewController {
 
             // day, index
             for course in classes {
-                if course.courseName == "" || course.arrange.first!.day > 5 || course.arrange.first!.day <= 0 || i < Int(course.weekStart)! || i > Int(course.weekEnd)! {
+                if course.courseName == "" || course.arrange.first!.day > 5 || course.arrange.first!.day <= 0 || i < Int(course.weekStart)! || i > Int(course.weekEnd)! || course.courseName.hasPrefix("[非本周]") {
                     continue
                 }
 
